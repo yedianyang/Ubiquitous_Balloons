ebc70876b5b3299fe4279022db6f84adc0c0971e
yedianyang <jimjguo@live.com>
2021-05-04 20:54:33 +0800

    ESP32 MQTT
    ESP32 MQTT

---
+++ 0_Arduino/ESP32_MQTT/config.h
@@ -0,0 +1,9 @@
+const char* ssid = "******";
+const char* password  = "******";
+
+const char* mqtt_server = "******";
+const int mqtt_port = 1883;
+const char* mqtt_user = "******";
+const char* mqtt_password = "******";
+
+const char* device_id = "******";
